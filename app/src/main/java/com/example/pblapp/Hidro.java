a9c484d42c1e28de4e9746d32ac977118b74ca28
awwaliahus <88544497+awwaliahus@users.noreply.github.com>
2023-12-07 07:04:07 +0700

    Update Hidro.java

--- app/src/main/java/com/example/pblapp/Hidro.java
+++ app/src/main/java/com/example/pblapp/Hidro.java
@@ -1,14 +1,28 @@
 package com.example.pblapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Hidro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hidro);
+        setContentView(R.layout.activity_penyiraman);
+
+        ImageButton arrowLeftButton = findViewById(R.id.arrow_left);
+        arrowLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tambahkan kode untuk kembali ke HomeActivity
+                Intent intent = new Intent(Penyiraman.this, HomeActivity.class);
+                startActivity(intent);
+                finish(); // Optional, untuk menutup activity saat ini dari stack
+            }
+        });
     }
 }
