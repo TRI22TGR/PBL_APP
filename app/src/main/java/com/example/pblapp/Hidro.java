93ffa5674e9e2d8a8160591475c4618376e1d476
TRI22TGR <tegarnrp@iet.student.pens.ac.id>
2023-12-08 14:33:54 +0700

    widiv4

--- app/src/main/java/com/example/pblapp/Hidro.java
+++ app/src/main/java/com/example/pblapp/Hidro.java
@@ -1,8 +1,18 @@
 package com.example.pblapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 
 public class Hidro extends AppCompatActivity {
 
@@ -10,5 +20,40 @@ public class Hidro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hidro);
+
+        ImageButton arrowLeftButton = findViewById(R.id.arrow_left);
+        arrowLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tambahkan kode untuk kembali ke HomeActivity
+                Intent intent = new Intent(Hidro.this, HomeActivity.class);
+                startActivity(intent);
+                finish(); // Optional, untuk menutup activity saat ini dari stack
+            }
+        });
+
+        // Generate and set a random value for Buttons and TextView
+        final Button button3 = findViewById(R.id.button3);
+        TextView hp1 = findViewById(R.id.hp1);
+
+        String randomValue = getRandomLastScanned();
+        button3.setText(randomValue);
+        hp1.setText(randomValue);
     }
+
+    // Helper method to generate random last scanned date
+    private String getRandomLastScanned() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+        Random random = new Random();
+
+        // Assuming you want a random date within the last year
+        long millis = System.currentTimeMillis() - random.nextInt(365) * 24 * 60 * 60 * 1000;
+
+        // Add random hours and minutes to the date
+        millis += random.nextInt(24) * 60 * 60 * 1000; // Hours
+        millis += random.nextInt(60) * 60 * 1000; // Minutes
+
+        return sdf.format(new Date(millis));
+    }
+
 }
--- app/src/main/java/com/example/pblapp/Lemon.java
+++ app/src/main/java/com/example/pblapp/Lemon.java
@@ -1,8 +1,17 @@
 package com.example.pblapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 
 public class Lemon extends AppCompatActivity {
 
@@ -10,5 +19,65 @@ public class Lemon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lemon);
+
+        ImageButton arrowLeftButton = findViewById(R.id.arrow_left);
+        arrowLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tambahkan kode untuk kembali ke HomeActivity
+                finish();
+            }
+        });
+        TextView txthp1 = findViewById(R.id.hp1);
+
+
+        // Generate random values for Buttons
+        Button buttonMS = findViewById(R.id.buttonMS);
+        Button buttonSM = findViewById(R.id.buttonSM);
+        Button buttonTB = findViewById(R.id.buttonTB);
+        final Button buttonLsc = findViewById(R.id.buttonLsc);
+
+        // Generate and set a random value for Buttons and TextView
+
+        TextView hp1 = findViewById(R.id.hp1);
+
+        String randomValue = getRandomLastScanned();
+        buttonLsc.setText(randomValue);
+        hp1.setText(randomValue);
+
+        // Set random values for Buttons
+        buttonMS.setText(String.valueOf(new Random().nextInt(10)));  // Random value for buttonMS
+        buttonSM.setText(String.valueOf(new Random().nextInt(10)));  // Random value for buttonSM
+        buttonTB.setText(String.valueOf(new Random().nextInt(120)));  // Random value for buttonTB
+
+        // Hitung total dan set pada buttonTB
+        int totalBuah = Integer.parseInt(buttonMS.getText().toString())
+                + Integer.parseInt(buttonSM.getText().toString());
+
+        buttonTB.setText(String.valueOf(totalBuah));
+    }
+
+
+
+
+    // Helper method to generate random status
+    private String getRandomStatus(String condition) {
+        String[] statuses = {"2", "6", "23"};  // Example random values for buttons
+        return condition + " " + statuses[new Random().nextInt(statuses.length)];
+    }
+
+
+    private String getRandomLastScanned() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+        Random random = new Random();
+
+        // Assuming you want a random date within the last year
+        long millis = System.currentTimeMillis() - random.nextInt(365) * 24 * 60 * 60 * 1000;
+
+        // Add random hours and minutes to the date
+        millis += random.nextInt(24) * 60 * 60 * 1000; // Hours
+        millis += random.nextInt(60) * 60 * 1000; // Minutes
+
+        return sdf.format(new Date(millis));
     }
 }
